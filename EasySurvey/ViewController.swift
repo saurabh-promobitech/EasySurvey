273027e905c849c6c6b7676b43133a0d7eca1f6e
ProMobi Technologies <promobi@ProMobis-MacBook-Air.local>
2016-05-18 20:00:50 +0530

    first commit

---
+++ EasySurvey/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EasySurvey
+//
+//  Created by ProMobi Technologies on 18/05/16.
+//  Copyright © 2016 Promobi Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
